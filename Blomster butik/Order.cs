f115208632b5caab6e3971205aa5afdbde320d01
Cupcake <Cupcake@DESKTOP-RTVVT6T>
2024-02-23 10:49:29 +0100

    Add project files.

---
+++ Blomster butik/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomster_butik
+{
+    internal class Order
+    {
+
+        int _id;
+        DateTime _date;
+        private static int idCounter = 0;
+
+
+        //properties
+        public int Id { get => _id; private set => _id = value; }
+        public DateTime Date { get => _date; private set => _date = value; }
+
+
+        //Constructer
+        public Order()
+        {
+            Id = idCounter;
+            Date = DateTime.Now;
+            idCounter++;
+        }
+
+        //overrider ToString
+        public override string ToString()
+        {
+            return "Order-id " + Id + " Date " + Date.ToString("dd-MM-yy : kl: HH:mm:ss:fff");
+        }
+
+    }
+}
---
+++ Blomster butik/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Blomster_butik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order o1 = new Order();
+            Console.WriteLine(o1.ToString());
+
+            Order o2 = new Order();
+            Console.WriteLine(o2.ToString());
+
+            Order o3 = new Order();
+            Console.WriteLine(o3.ToString());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
